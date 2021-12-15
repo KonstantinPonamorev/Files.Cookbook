4823cab195e0b84361ddccfe3500e9bca209bcbb
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2021-12-15 21:45:24 +0300

    Task 3. All done

--- main.py
+++ main.py
@@ -32,13 +32,57 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 # get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
 
-def sort_file_by_strings(number_of_file):
-    max_strings = 0
-    file_with_max_strings = 0
-    for number in range(1, int(number_of_file) + 1):
-        with open(f'{number}.txt', encoding='utf-8') as file:
-            if len(file.readlines()) > max_strings:
-                max_strings = len(file.readlines())
-                file_with_max_strings = number
 
-sort_file_by_strings(3)
+def sort_files_by_string(file_name_1, file_name_2, file_name_3):
+    with open(file_name_1, encoding='utf-8') as file_1:
+        with open(file_name_2, encoding='utf-8') as file_2:
+            with open(file_name_3, encoding='utf-8') as file_3:
+                sorted_length = ['','','']
+                len_file_1 = len(file_1.readlines())
+                len_file_2 = len(file_2.readlines())
+                len_file_3 = len(file_3.readlines())
+                if len_file_1 < len_file_2 and len_file_1 < len_file_3:
+                    sorted_length[0] = file_name_1
+                    if len_file_2 < len_file_3:
+                        sorted_length[1] = file_name_2
+                        sorted_length[2] = file_name_3
+                    else:
+                        sorted_length[1] = file_name_3
+                        sorted_length[2] = file_name_2
+                elif len_file_2 < len_file_3:
+                    sorted_length[0] = file_name_2
+                    if len_file_1 < len_file_3:
+                        sorted_length[1] = file_name_1
+                        sorted_length[2] = file_name_3
+                    else:
+                        sorted_length[1] = file_name_3
+                        sorted_length[2] = file_name_1
+                else:
+                    sorted_length[0] = file_name_3
+                    if len_file_1 < len_file_2:
+                        sorted_length[1] = file_name_1
+                        sorted_length[2] = file_name_2
+                    else:
+                        sorted_length[1] = file_name_2
+                        sorted_length[2] = file_name_1
+    with open('sortedtext.txt', 'w', encoding='utf-8') as new_file:
+        with open(sorted_length[0], encoding='utf-8') as file_by_string_1:
+            new_file.write(f'{sorted_length[0]}\n')
+            new_file.write(f'{str(len(file_by_string_1.readlines()))}\n')
+            file_by_string_1.seek(0)
+            for line in file_by_string_1:
+                new_file.write(line)
+        with open(sorted_length[1], encoding='utf-8') as file_by_string_2:
+            new_file.write(f'\n{sorted_length[1]}\n')
+            new_file.write(f'{str(len(file_by_string_2.readlines()))}\n')
+            file_by_string_2.seek(0)
+            for line in file_by_string_2:
+                new_file.write(line)
+        with open(sorted_length[2], encoding='utf-8') as file_by_string_3:
+            new_file.write(f'\n{sorted_length[2]}\n')
+            new_file.write(f'{str(len(file_by_string_3.readlines()))}\n')
+            file_by_string_3.seek(0)
+            for line in file_by_string_3:
+                new_file.write(line)
+
+# sort_files_by_string('1.txt', '2.txt', '3.txt')
